f7f2e65c821f657d96ac722b1c347b265a4fc0e2
YassineIdiri <pro.yassine.idiri@gmail.com>
2026-01-25 01:52:25 +0100

    update name

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('smart-expense-tracker');
+  protected readonly title = signal('expense-tracker');
 }
